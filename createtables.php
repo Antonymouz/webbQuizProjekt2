3b92be2aa8321e1c70f1c50b275ecddd9ac2d93e
Anton Eckervald <antoned@varmdogymnasium.se>
2021-01-23 17:00:29 +0100

    first commit

---
+++ createtables.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+		include(',,/dbconnection.php');
+		try{
+			$sql = "CREATE TABLE user(
+				name 
+			)"
+		}
+	?>
+</body>
+</html>
---
+++ quiz.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Välj quiz</title>
+</head>
+<body>
+
+	<select name="quiz">
+
+	</select>
+
+</body>
+</html>
